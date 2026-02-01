23ec1aeaef61931e731971c6a6296f439c72b7aa
Yayasan At-Tauhid <yayasan.attauhid.1@gmail.com>
2026-02-02 03:10:03 +0700

    update

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -2,8 +2,8 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
-const SUPABASE_URL = "https://sonnfclnzsasjifieuog.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNvbm5mY2xuenNhc2ppZmlldW9nIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjgzNjAyNzQsImV4cCI6MjA4MzkzNjI3NH0.ihx1oJr8PMcEki_mx1t6qpYdEjTRxeqru2KCryzYOvo";
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
+const SUPABASE_PUBLISHABLE_KEY = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
 
 // Import the supabase client like this:
 // import { supabase } from "@/integrations/supabase/client";
