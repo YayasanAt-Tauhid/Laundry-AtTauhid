18e2f1d57e9f1540d33b7a4de958fd75da1cd8e7
Yayasan At-Tauhid <yayasan.attauhid.1@gmail.com>
2026-02-02 04:19:35 +0700

    delete

--- supabase/migrations/20251223042100_add_laundry_date.sql
+++
@@ -1,6 +0,0 @@
--- Add laundry_date column to laundry_orders table
-ALTER TABLE public.laundry_orders 
-ADD COLUMN laundry_date DATE NOT NULL DEFAULT CURRENT_DATE;
-
--- Add comment to describe the column
-COMMENT ON COLUMN public.laundry_orders.laundry_date IS 'Tanggal siswa mengikuti laundry';
