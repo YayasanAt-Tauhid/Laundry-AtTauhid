cc4702f9b150c9c50760823d3b55f98819fe57a2
Yayasan At-Tauhid <yayasan.attauhid.1@gmail.com>
2026-02-09 14:22:45 +0700

    update

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -6,9 +6,6 @@ console.log("URL =", import.meta.env.VITE_SUPABASE_URL)
 console.log("PUB =", import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY)
 console.log("ALL =", import.meta.env)
 
-
-
-
 const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
 const SUPABASE_PUBLISHABLE_KEY = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
 
