4516091991c3631f4ab3dc62879d43428862d7f3
Yayasan At-Tauhid <yayasan.attauhid.1@gmail.com>
2026-02-09 16:22:48 +0700

    update

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -13,7 +13,7 @@ if (!SUPABASE_URL || !SUPABASE_KEY) {
   throw new Error('Supabase ENV missing')
 }
 
-export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY, {
+export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_KEY, {
   auth: {
     storage: localStorage,
     persistSession: true,
