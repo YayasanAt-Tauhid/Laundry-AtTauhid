cdaac3c8094eb4db9b4bfa730797f03c0b0395f3
Yayasan At-Tauhid <yayasan.attauhid.1@gmail.com>
2026-02-09 13:43:35 +0700

    update

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -2,6 +2,10 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
+console.log("URL =", import.meta.env.VITE_SUPABASE_URL)
+console.log("PUB =", import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY)
+console.log("ALL =", import.meta.env)
+
 const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
 const SUPABASE_PUBLISHABLE_KEY = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
 
