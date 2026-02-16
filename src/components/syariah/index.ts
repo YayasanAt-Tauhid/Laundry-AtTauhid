960809e0aa2bad5d82552d8c5abc7f8d72685ecd
Yayasan At-Tauhid <yayasan.attauhid.1@gmail.com>
2026-02-16 14:41:03 +0700

    delete

--- src/components/syariah/index.ts
+++
@@ -1,5 +0,0 @@
-export { SyariahPolicyInfo } from "./SyariahPolicyInfo";
-export { WadiahBalanceCard } from "./WadiahBalanceCard";
-export { SyariahPaymentDialog } from "./SyariahPaymentDialog";
-export { WadiahDepositDialog } from "./WadiahDepositDialog";
-export type { SyariahPaymentData } from "./SyariahPaymentDialog";
--- supabase/migrations/20260214095829_0f8ac552-333a-4dea-b9d5-5e3659b99fdf.sql
+++
@@ -1,2 +0,0 @@
--- Remove the policy that allows parents to delete students
-DROP POLICY IF EXISTS "Parents can delete own students" ON public.students;
